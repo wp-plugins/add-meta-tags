1c8ad2a542f6055d96e9dfd2164601d2597e3d63
gnotaras <gnotaras@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-21 23:13:39 +0000

    Tagging the 2.3.2 release
    git-svn-id: https://plugins.svn.wordpress.org/add-meta-tags/tags/2.3.2@791546 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ amt-template-tags.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Module containing template tags.
+ */
+
+
+function amt_content_description() {
+    $post = get_queried_object();
+    echo amt_get_content_description($post);
+}
+
+function amt_content_keywords() {
+    $post = get_queried_object();
+    echo amt_get_content_keywords($post);
+}
+
+function amt_content_keywords_mesh() {
+    $post = get_queried_object();
+    // Keywords echoed in the form: keyword1;keyword2;keyword3
+    echo amt_get_content_keywords_mesh($post);
+}
+
+function amt_metadata() {
+    // Prints full metadata.
+    amt_add_metadata();
+}
+
+function amt_metadata_review() {
+    // Prints full metadata in review mode. No user level checks here.
+    echo amt_get_metadata_inspect();
+}
+
